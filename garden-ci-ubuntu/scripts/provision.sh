911d640fd6a0e6e40d5d7fc5160202b4fcd1e068
Ed King <eking@pivotal.io>
2016-09-14 16:08:35 +0100

    Ensure netcat is installed in CI images
    [#129000661]

--- garden-ci-ubuntu/scripts/provision.sh
+++ garden-ci-ubuntu/scripts/provision.sh
@@ -21,7 +21,8 @@ apt-get -y --force-yes install \
   ulogd \
   pkg-config \
   libapparmor-dev \
-  apparmor-utils
+  apparmor-utils \
+  netcat
 
 # seccomp profiles require a recent (>= 2.2.1) version of seccomp
 echo 'deb http://httpredir.debian.org/debian jessie-backports main' > /etc/apt/sources.list.d/backports.list
