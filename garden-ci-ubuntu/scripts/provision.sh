8b4ac3507938a70437e8ccc196fde64ead9599bf
Ed King <eking@pivotal.io>
2017-02-01 16:30:13 +0000

    Update garden-ci-ubuntu golang to v1.7.5
    [#138600301]

--- garden-ci-ubuntu/scripts/provision.sh
+++ garden-ci-ubuntu/scripts/provision.sh
@@ -32,8 +32,8 @@ apt-get -y --force-yes install \
   libseccomp-dev/jessie-backports
 rm /etc/apt/sources.list.d/backports.list
 
-# install go1.7.1
-wget -qO- https://storage.googleapis.com/golang/go1.7.1.linux-amd64.tar.gz | tar -C /usr/local -xzf -
+# install go1.7.5
+wget -qO- https://storage.googleapis.com/golang/go1.7.5.linux-amd64.tar.gz | tar -C /usr/local -xzf -
 
 go get github.com/onsi/ginkgo/ginkgo
 go install github.com/onsi/ginkgo/ginkgo
