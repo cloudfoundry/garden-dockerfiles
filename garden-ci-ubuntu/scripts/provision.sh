856294ff3a46c9b606dbb6b52b78d649c292a098
Craig Furman <cfurman@pivotal.io>
2017-02-17 14:25:09 +0000

    garden-ci-ubuntu v0.7.0
    update Go to 1.8
    
    [#140096135]

--- garden-ci-ubuntu/scripts/provision.sh
+++ garden-ci-ubuntu/scripts/provision.sh
@@ -32,8 +32,7 @@ apt-get -y --force-yes install \
   libseccomp-dev/jessie-backports
 rm /etc/apt/sources.list.d/backports.list
 
-# install go1.7.5
-wget -qO- https://storage.googleapis.com/golang/go1.7.5.linux-amd64.tar.gz | tar -C /usr/local -xzf -
+wget -qO- https://storage.googleapis.com/golang/go1.8.linux-amd64.tar.gz | tar -C /usr/local -xzf -
 
 go get github.com/onsi/ginkgo/ginkgo
 go install github.com/onsi/ginkgo/ginkgo
