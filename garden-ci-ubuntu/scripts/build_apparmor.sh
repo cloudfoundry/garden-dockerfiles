a356a516499190fedcaccff1c924e1c1e7d2d450
Topher Bullock <cbullock@pivotal.io>
2017-10-24 14:39:26 +0100

    Bump apparmor in garden-ci-ubuntu test image
    tag = 0.10.3
    
    [#152144001]
    
    Signed-off-by: Topher Bullock <cbullock@pivotal.io>
    Signed-off-by: Claudia Beresford <cberesford@pivotal.io>

--- garden-ci-ubuntu/scripts/build_apparmor.sh
+++ garden-ci-ubuntu/scripts/build_apparmor.sh
@@ -9,28 +9,28 @@ cd $temp_dir_path
 apt-get update
 apt-get install wget build-essential autoconf libtool gettext -y
 
-wget "https://launchpad.net/apparmor/2.11/2.11/+download/apparmor-2.11.0.tar.gz"
+wget "https://launchpad.net/apparmor/2.11/2.11.1/+download/apparmor-2.11.1.tar.gz"
 
-tar zxf apparmor-2.11.0.tar.gz
+tar zxf apparmor-2.11.1.tar.gz
 
-pushd apparmor-2.11.0/libraries/libapparmor
+pushd apparmor-2.11.1/libraries/libapparmor
   sh ./autogen.sh
   sh ./configure --prefix=/usr
   make
   make install
 popd
 
-pushd apparmor-2.11.0/binutils
+pushd apparmor-2.11.1/binutils
   make
   make install
 popd
 
-pushd apparmor-2.11.0/utils
+pushd apparmor-2.11.1/utils
   make
   make install
 popd
 
-pushd apparmor-2.11.0/parser
+pushd apparmor-2.11.1/parser
   make
   make install
 popd
